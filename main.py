377118fec9e90a68488e8d74fbf89c33c6f70bf4
pablopalillo <pablo.576@hotmail.com>
2022-08-30 22:34:52 -0500

    Add directory conf and jinja conf

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
 from utils import get_my_ip
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
 async def root():
 
     my_ip = get_my_ip()
-    return {"message": my_ip}
+    return templates.TemplateResponse('index.html', {"message": my_ip})
